036caeff275daa3ba0744622a3c1305f6031cae6
Kevin ortega <milife.ortega2000@gmail.com>
2025-04-12 14:09:44 -0500

    Actualización de comandos y normalización de saltos de línea

--- src/scripts/tunnel.js
+++ src/scripts/tunnel.js
@@ -2,6 +2,8 @@
 
 import { spawn } from 'child_process';
 import chalk from 'chalk';
+// Alternativa si hay problemas con chalk ESM
+// const chalk = { green: (text) => `\x1b[32m${text}\x1b[0m`, blue: (text) => `\x1b[34m${text}\x1b[0m`, yellow: (text) => `\x1b[33m${text}\x1b[0m`, magenta: (text) => `\x1b[35m${text}\x1b[0m`, red: (text) => `\x1b[31m${text}\x1b[0m`, gray: (text) => `\x1b[90m${text}\x1b[0m`, bold: { underline: (text) => `\x1b[1m\x1b[4m${text}\x1b[0m` } };
 import clipboardy from 'clipboardy';
 import { createServer } from 'vite';
 
--- tunnelMessages.js
+++ tunnelMessages.js
@@ -1,5 +1,7 @@
 // tunnelMessages.js
 import chalk from 'chalk';
+// Alternativa si hay problemas con chalk ESM
+// const chalk = { green: (text) => `\x1b[32m${text}\x1b[0m`, yellow: (text) => `\x1b[33m${text}\x1b[0m`, red: (text) => `\x1b[31m${text}\x1b[0m` };
 
 export const displayMessages = () => {
   console.log(chalk.green('✅ Túnel creado exitosamente'));
